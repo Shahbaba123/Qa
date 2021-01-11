8c3f2ae29cebd93b93e75841dd7de26bc300c2de
Shahbaba123 <76240932+Shahbaba123@users.noreply.github.com>
2021-01-10 22:13:39 -0500

    first commit
     uploading documents

---
+++ Area/src/identifier.java
@@ -0,0 +1,13 @@
+
+public class identifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+short a=1;
+short b=2;
+short c=3;
+System.out.println(a+b+c);
+
+	}
+
+}
